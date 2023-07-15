e737e6cb6c741004815e5eafce52397e2a04c369
Benedikt Schächner <137323528+SchBenedikt@users.noreply.github.com>
2023-07-15 10:38:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,82 +1,88 @@
 // Default username
 const defaultUsername = "schBenedikt";
 
-// Function to fetch GitHub project data using the API
+// Function to get the GitHub project data via API
 async function getGitHubProjects(username) {
-  const response = await fetch(`https://api.github.com/users/${username}/repos`);
-  const data = await response.json();
-  return data;
+   const response = await fetch(`https://api.github.com/users/${username}/repos`);
+   const data = await response.json();
+   return data;
 }
 
-// Function to create project cards based on project data
+// Function to create the project maps based on the project data
 function createProjectCards(projects) {
-  const projectsContainer = document.querySelector(".projects");
+   const projectsContainer = document.querySelector(".projects");
 
-  projects.forEach((project) => {
-    const projectCard = document.createElement("div");
-    projectCard.classList.add("project");
-    projectCard.onclick = () => openOverlay(project);
+   projects.forEach((project) => {
+     const projectCard = document.createElement("div");
+     projectCard.classList.add("project");
+     projectCard.onclick = () => openOverlay(project);
 
-    const title = document.createElement("h2");
-    title.textContent = project.name;
+     const title = document.createElement("h2");
+     title.textContent = project.name;
 
-    const description = document.createElement("p");
-    description.textContent = project.description;
+     const description = document.createElement("p");
+     description.textContent = project.description;
 
-    const viewLink = document.createElement("a");
-    viewLink.href = project.html_url;
-    viewLink.textContent = "View Project";
-    viewLink.target = "_blank"; // Open link in a new tab
+const viewLink = document.createElement("a");
+viewLink.href = project.html_url;
+viewLink.textContent = "View Project";
+viewLink.target = "_blank"; // Open link in a new tab
 
-    projectCard.appendChild(title);
-    projectCard.appendChild(description);
-    projectCard.appendChild(viewLink);
+projectCard.appendChild(title);
+projectCard.appendChild(description);
+projectCard.appendChild(viewLink);
 
-    projectsContainer.appendChild(projectCard);
-  });
+projectsContainer.appendChild(projectCard);
+   });
 }
 
-// Function to fetch the content of a project's README.html file
+// Function to get the contents of a project's README.md file
 async function getReadmeContent(username, repoName) {
-  const response = await fetch(`https://raw.githubusercontent.com/${username}/${repoName}/main/README.html`);
-  const data = await response.text();
-  return data;
+   const response = await fetch(`https://api.github.com/repos/${username}/${repoName}/contents/README.md`);
+   const data = await response.json();
+
+   if (data.encoding === "base64") {
+     const readmeContent = atob(data.content); // Base64 decode the content
+     return readmeContent;
+   } else {
+     throw new Error("README.md file not found.");
+   }
 }
 
-// Function to open the overlay with the content of the README.html file
+// Function to open the overlay with the contents of the README.md file
 async function openOverlay(project) {
-  const overlay = document.getElementById("overlay");
-  const overlayTitle = document.getElementById("overlay-title");
-  const overlayReadme = document.getElementById("overlay-readme");
+   const overlay = document.getElementById("overlay");
+   const overlayTitle = document.getElementById("overlay-title");
+   const overlayReadme = document.getElementById("overlay-readme");
 
-  overlayTitle.textContent = project.name;
+   overlayTitle.textContent = project.name;
 
-  try {
-    // Fetch README.html content
-    const readmeContent = await getReadmeContent(defaultUsername, project.name);
+   try {
+     // Get README.md content
+     const readmeContent = await getReadmeContent(defaultUsername, project.name);
 
-    // Insert the content into the overlay
-    overlayReadme.innerHTML = readmeContent;
+     // Paste the content into the overlay
+     overlayReadme.textContent = readmeContent;
 
-    overlay.classList.add("active");
-  } catch (error) {
-    console.error("Error fetching README.html content:", error);
-  }
+     overlay.classList.add("active");
+   } catch (error) {
+     console.error("Error getting README.md content:", error);
+   }
 }
 
 // Function to close the overlay
 function closeOverlay() {
-  const overlay = document.getElementById("overlay");
-  overlay.classList.remove("active");
+   const overlay = document.getElementById("overlay");
+   overlay.classList.remove("active");
 }
 
-// Example call to the functions
+// example call of the functions
 const username = defaultUsername;
 
 getGitHubProjects(username)
-  .then((projects) => {
-    createProjectCards(projects);
-  })
-  .catch((error) => {
-    console.error("Error fetching GitHub projects:", error);
-  });
+   .then((projects) => {
+     createProjectCards(projects);
+   })
+   .catch((error) => {
+     console.error("Error getting GitHub projects:", error);
+   });
