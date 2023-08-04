ca99eba1f6484b70c712f313e8c4d26eca9a32dc
Benedikt Schächner <benedikt.schaechner@web.de>
2023-08-04 13:41:53 +0200

    Create settings.js

---
+++ js/settings.js
@@ -0,0 +1,54 @@
+
+    function saveBackgroundColor(color) {
+      document.cookie = `background_color=${encodeURIComponent(color)}; expires=Fri, 31 Dec 9999 23:59:59 GMT;`;
+    }
+
+    function getBackgroundColor() {
+      const cookieValue = document.cookie.replace(/(?:(?:^|.*;\s*)background_color\s*=\s*([^;]*).*$)|^.*$/, "$1");
+      return decodeURIComponent(cookieValue) || "#ededed";
+    }
+
+    function applyBackgroundColor() {
+      const body = document.body;
+      const backgroundColor = getBackgroundColor();
+      body.style.backgroundColor = backgroundColor;
+    }
+
+    function saveButtonColor(color) {
+      document.cookie = `button_color=${encodeURIComponent(color)}; expires=Fri, 31 Dec 9999 23:59:59 GMT;`;
+    }
+
+    function getButtonColor() {
+      const cookieValue = document.cookie.replace(/(?:(?:^|.*;\s*)button_color\s*=\s*([^;]*).*$)|^.*$/, "$1");
+      return decodeURIComponent(cookieValue) || "#333";
+    }
+
+    function applyButtonColor() {
+      const buttons = document.querySelectorAll(".button");
+      const buttonColor = getButtonColor();
+      buttons.forEach(button => {
+        button.style.backgroundColor = buttonColor;
+        button.style.color = "#fff"; // To ensure the text is visible on the colored button
+      });
+    }
+
+    function saveProjectColor(color) {
+      document.cookie = `project_color=${encodeURIComponent(color)}; expires=Fri, 31 Dec 9999 23:59:59 GMT;`;
+    }
+
+    function getProjectColor() {
+      const cookieValue = document.cookie.replace(/(?:(?:^|.*;\s*)project_color\s*=\s*([^;]*).*$)|^.*$/, "$1");
+      return decodeURIComponent(cookieValue) || "#f8f8f8";
+    }
+
+    function applyProjectColor() {
+      const projects = document.querySelectorAll(".project");
+      const projectColor = getProjectColor();
+      projects.forEach(project => {
+        project.style.backgroundColor = projectColor;
+      });
+    }
+
+    applyBackgroundColor();
+    applyButtonColor();
+    applyProjectColor();
